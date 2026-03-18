4fcc80227c8af4630ff38de6c8c47dd0d717dc99
Alex <a.menkoff@gmail.com>
2026-03-19 00:48:36 +0300

    fix styling

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import { getBtnText } from "./modules.js"
 import { renderMovies } from "./render.js"
 
 const searchForm = document.getElementById('search-box')
+const placeholderEl = document.getElementById('placeholder')
+const searchBtn = document.getElementById('search-movie-btn')
+const originalBtnText = searchBtn.textContent
 
 
 let moviesIds = []
@@ -22,7 +25,13 @@ document.addEventListener('submit', function (e) {
     e.preventDefault()
 
     const formData = new FormData(searchForm)
+
+    searchBtn.textContent = 'Loading...'
+
+    searchBtn.disabled = true
+
     getMoviesByTitle(formData.get('search-movie'))
+
 
 })
 
@@ -52,6 +61,9 @@ async function getMoviesByTitle(movieTitle) {
 
     if (!totalPages) {
         console.log("Error getting total pages", data)
+        placeholderEl.innerHTML = `<h3>Unable to find what you’re looking for. Please try another search.</h3>`
+        searchBtn.disabled = false
+        searchBtn.textContent = originalBtnText
         return
     }
 
@@ -69,6 +81,8 @@ async function getMoviesIds(searchUrl, totalPages, movieTitle) {
         console.log(data)
 
         if (data.Response === "False") {
+            searchBtn.disabled = false
+            searchBtn.textContent = originalBtnText
             return
         }
 
@@ -77,54 +91,17 @@ async function getMoviesIds(searchUrl, totalPages, movieTitle) {
         }
     }
 
-    document.getElementById('placeholder').remove()
+    if (document.getElementById('placeholder')) {
+        document.getElementById('placeholder').remove()
+    }
+
+
 
     renderMovies(moviesIds, savedWatchList)
+
+    searchBtn.textContent = originalBtnText
+    searchBtn.disabled = false
+
 }
 
 
-
-
-// {Response: 'False', Error: 'Too many results.'}
-
-
-// {
-//     "Title": "Blade Runner",
-//     "Year": "1982",
-//     "Rated": "R",
-//     "Released": "25 Jun 1982",
-//     "Runtime": "117 min",
-//     "Genre": "Action, Drama, Sci-Fi",
-//     "Director": "Ridley Scott",
-//     "Writer": "Hampton Fancher, David Webb Peoples, Philip K. Dick",
-//     "Actors": "Harrison Ford, Rutger Hauer, Sean Young",
-//     "Plot": "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
-//     "Language": "English, German, Cantonese, Japanese, Hungarian, Arabic, Korean",
-//     "Country": "United States, United Kingdom, Hong Kong",
-//     "Awards": "Nominated for 2 Oscars. 13 wins & 22 nominations total",
-//     "Poster": "https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg",
-//     "Ratings": [
-//         {
-//             "Source": "Internet Movie Database",
-//             "Value": "8.1/10"
-//         },
-//         {
-//             "Source": "Rotten Tomatoes",
-//             "Value": "89%"
-//         },
-//         {
-//             "Source": "Metacritic",
-//             "Value": "84/100"
-//         }
-//     ],
-//     "Metascore": "84",
-//     "imdbRating": "8.1",
-//     "imdbVotes": "876,371",
-//     "imdbID": "tt0083658",
-//     "Type": "movie",
-//     "DVD": "N/A",
-//     "BoxOffice": "$32,914,489",
-//     "Production": "N/A",
-//     "Website": "N/A",
-//     "Response": "True"
-// }
